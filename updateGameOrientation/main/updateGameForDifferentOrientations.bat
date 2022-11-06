27b653be62a13736e507a42153551d3ea9ed6ffd
sketer amrit <ksamrit121@gmail.com>
2022-11-06 19:24:59 +0530

    remove hardcore gameName and localization path

--- updateGameOrientation/main/updateGameForDifferentOrientations.bat
+++ updateGameOrientation/main/updateGameForDifferentOrientations.bat
@@ -1,20 +1,21 @@
 @echo off
 
-:: delete main_data.json from game folder and desktop_en_main_data.json from Localization Tool (you may change the Localization Tools path or game name)
+:: delete main_data.json from game folder and desktop_en_main_data.json from Localization Tool (Localization Tools path or game name should be provide by another file which start this script)
+:: Here %1 represent GameName and %2 represent Localization Tool path
 D:
-cd D:\programming\tools\LocalizationTool(core gaming)\resource\destop_EN_JSON\
+cd %2\resource\destop_EN_JSON\
 del desktop_en_main_data.json
 echo "Delete desktop_en_main_data.json from Localization Tool"
 cd D:\IngenuityGit\ONLINE\CoreGaming\%1
 del main_data.json
 
 :: copy desktop_en_main_data.json from game to Localization Tool and execute replicateMainDataGb command
-copy "D:\IngenuityGit\ONLINE\CoreGaming\%1\template\source\res\manifest\mainData\desktop\en\desktop_en_main_data.json" "D:\programming\tools\LocalizationTool(core gaming)\resource\destop_EN_JSON\"
-cd D:\programming\tools\LocalizationTool(core gaming)
+copy "D:\IngenuityGit\ONLINE\CoreGaming\%1\template\source\res\manifest\mainData\desktop\en\desktop_en_main_data.json" "%2\resource\destop_EN_JSON\"
+cd %2
 node replicateMainDataGB.js
 
 :: after executing the command move main_data.json to game folder
-move "D:\programming\tools\LocalizationTool(core gaming)\resource\mainDataTarget\main_data.json" "D:\IngenuityGit\ONLINE\CoreGaming\%1\"
+move "%2\resource\mainDataTarget\main_data.json" "D:\IngenuityGit\ONLINE\CoreGaming\%1\"
 echo "update main_data.json sucessfully"
 echo "Automatically exiting, please wait"
 
@@ -22,4 +23,4 @@ echo "Automatically exiting, please wait"
 timeout /t 5 /nobreak
 
 :: pause command prompt after script completed
-pause
+::pause
