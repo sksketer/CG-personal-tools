2b5b9f1b608f3343684c82a9252218f1c713cb96
sketer amrit <ksamrit121@gmail.com>
2022-11-06 19:26:17 +0530

    change gamename and localization tool path

--- updateGameOrientation/replicateGame.bat
+++ updateGameOrientation/replicateGame.bat
@@ -1,6 +1,9 @@
 echo off
 
-updateGameForDifferentOrientations elephantgold
+c:
+cd C:\Users\ksamr\CG-personal-tools\updateGameOrientation\main\
+:: Here you may change the GameName and Localization Tool path
+updateGameForDifferentOrientations elephantgold D:\programming\tools\LocalizationTool(coregaming)
 
 
 
